067ffe30a5964e18624cc676be253834ea36a090
CapnSammehj <samherring111@gmail.com>
2019-06-24 20:10:29 +1000

    Revert "Update server.js"
    This reverts commit 7a9cbb93a37fddde2d43b6c8ecb1312eb57a7357.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const randomWord = require('random-words');
 const express = require('express');
 const app = express();
+app.set('view engine', 'ejs')
 
 let searchResult = 'https://au.answers.yahoo.com/search/search_result?fr=uh3_answers_vert_gs&type=2button&p=' + randomWord();
 let answerURL = "https://au.answers.yahoo.com/question/index?qid=";
@@ -24,16 +25,9 @@ rp(searchResult)
     result = answerURL + _.sample(answers);
   });
 
-const router = express.Router();
-router.get('/', (req, res) =>{
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1 style="text-align: center">Yahoo Answers Randomizer</h1><div style="text-align: center; font-size: 20"><a href=<%= url %>>Yahoo Answers URL</a></div>');
-  res.end();
+app.get('/', function(req, res){
+  res.render('index', {url: result});
 })
-
-router.post('/', (req, res) => res.json({ postBody: req.body }));
-
-app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 app.listen(3000, function(){
   console.log("App Running on Port 3000!");
